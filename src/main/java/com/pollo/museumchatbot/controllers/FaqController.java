a98ab3349c194dcdd3ce70ad3cf6e5db3233cdf7
Renan Pollo Benelli <renansk18@outlook.com>
2024-05-01 18:45:16 -0300

    feat: add cors configuration

--- src/main/java/com/pollo/museumchatbot/controllers/FaqController.java
+++ src/main/java/com/pollo/museumchatbot/controllers/FaqController.java
@@ -5,12 +5,14 @@ import com.pollo.museumchatbot.dto.MessageResponse;
 import com.pollo.museumchatbot.service.FaqService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("api/chat")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class FaqController {
     final private FaqService faqService;
 
