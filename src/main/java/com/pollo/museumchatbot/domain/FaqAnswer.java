8e900fc3617ed867057b1195b887bc297f4a0302
Renan Pollo Benelli <renansk18@outlook.com>
2024-05-01 16:54:43 -0300

    refactor: add Lombok getter and removed other methods

--- src/main/java/com/pollo/museumchatbot/domain/FaqAnswer.java
+++ src/main/java/com/pollo/museumchatbot/domain/FaqAnswer.java
@@ -1,28 +1,13 @@
 package com.pollo.museumchatbot.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class FaqAnswer {
-
     private List<String> keywords;
-
     private String answer;
-
-    public FaqAnswer(List<String> keywords, String answer) {}
-
-    public List<String> getKeywords() {
-        return keywords;
-    }
-
-    public void setKeywords(List<String> keywords) {
-        this.keywords = keywords;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
 }
